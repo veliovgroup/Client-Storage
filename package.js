2bafd2495c1e5ae0793ccb0397cab5106fdb0c73
dr.dimitru <ceo@veliov.com>
2022-05-31 14:00:11 +0300

    📦 v4.0.1
    - 👨‍💻 Support server environment
    - 👨‍🔬 Improve tests

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:cstorage',
-  version: '4.0.0',
+  version: '4.0.1',
   summary: 'Bulletproof persistent Client (Browser) storage, works with disabled Cookies and/or localStorage',
   git: 'https://github.com/VeliovGroup/Client-Storage',
   documentation: 'README.md'
@@ -8,12 +8,12 @@ Package.describe({
 
 Package.onUse((api) => {
   api.versionsFrom('1.4');
-  api.use('ecmascript', 'client');
-  api.mainModule('client-storage.js', 'client');
+  api.use('ecmascript');
+  api.mainModule('client-storage.js');
 });
 
 Package.onTest((api) => {
   api.use('tinytest');
-  api.use('ecmascript', 'client');
-  api.addFiles('client-storage-tests.js', 'client');
+  api.use('ecmascript');
+  api.addFiles('client-storage-tests.js');
 });
