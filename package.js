260d3da1be6cd6fd1d444aa97476bbbb6025d70f
dr.dimitru <ceo@veliov.com>
2026-04-26 22:47:12 +0300

    👨‍💻 add: meteor.js typescript support

--- package.js
+++ package.js
@@ -7,13 +7,14 @@ Package.describe({
 });
 
 Package.onUse((api) => {
-  api.versionsFrom('1.4');
+  api.versionsFrom(['1.4', '2.8.0', '3.0.1', '3.4']);
   api.use('ecmascript');
+  api.use(['typescript', 'zodern:types@1.0.13'], ['client', 'server'], { weak: true });
+  api.addAssets('index.d.ts', ['server', 'client']);
   api.mainModule('client-storage.js');
 });
 
 Package.onTest((api) => {
-  api.use('tinytest');
-  api.use('ecmascript');
+  api.use(['tinytest', 'ecmascript', 'zodern:types', 'typescript'], ['client', 'server']);
   api.addFiles('client-storage-tests.js');
 });
